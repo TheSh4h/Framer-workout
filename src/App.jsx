4986017d6100e44f36dd0c3dd0cd6f578121f1fc
TheSh4h <hh.haseebshah@gmail.com>
2024-03-08 02:24:44 +0500

    initiated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import BasicsOfMotion from './components/BasicsOfMotion'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <div>
+      <BasicsOfMotion />
+    </div>
+  )
+}
+
+export default App
