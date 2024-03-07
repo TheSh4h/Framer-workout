efa8118e65515d4bbf2fcb11f7e56ba1448608eb
TheSh4h <hh.haseebshah@gmail.com>
2024-03-08 02:50:45 +0500

    Gestures done

--- src/components/Gestures.jsx
+++ src/components/Gestures.jsx
@@ -1,7 +1,36 @@
+import { MotionConfig, motion } from "framer-motion";
+
 const Gestures = () => {
     return ( 
-        <div>
-            
+        <div 
+            style={{
+                display: "grid",
+                placeContent: "center",
+                height: "100vh",
+                gap: "0.8rem",
+            }}
+        >
+            <MotionConfig
+              transition={{
+                duration: 0.125,
+                ease: "easeInOut",
+            }}
+            >
+                <motion.button
+                whileHover={{scale: 1.05}}
+                whileTap={{scale: 0.95, rotate: "2.5deg"}}
+                className="example-button">
+                    Click Me
+                </motion.button>
+
+                <motion.button
+                whileHover={{scale: 1.15}}
+                whileTap={{scale: 0.85, rotate: "-2.5deg"}}
+                style={{backgroundColor: "red"}}
+                className="example-button">
+                    Click Me
+                </motion.button>
+            </MotionConfig>
         </div>
      );
 }
