6637acb29feca8da851f06a40b4bc9ffe8f4d681
TheSh4h <hh.haseebshah@gmail.com>
2024-03-09 01:03:40 +0500

    view based animation done

--- src/components/ViewBasedAnimations.jsx
+++ src/components/ViewBasedAnimations.jsx
@@ -1,13 +1,32 @@
+import { motion, useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
+
 const ViewBasedAnimations = () => {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true});
+
+    useEffect(() => {
+        console.log("is in view ->", isInView)
+    }, [isInView])
+
     return ( 
-        <div
-          style={{
-            display: "grid",
-            placeContent: "center",
-            height: "100vh",
-            gap: "0.8rem",
-          }}
-        ></div>
+        <>
+          <div style={{height: "150vh"}} />
+          <motion.div 
+            style={{ height: "100vh", background: "black "}}
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{ duration: 1 }}
+          />
+          <div
+            ref={ref}
+            style={{
+              height: "100vh", 
+              background: isInView ? "blue": "red",
+              transition: "2s background",
+            }}
+          />
+        </>
      );
 }
  
